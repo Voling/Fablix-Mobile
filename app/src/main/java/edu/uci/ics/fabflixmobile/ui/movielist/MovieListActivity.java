0f639df76f72d4803b23ab6b5d8275cdd1b87f46
Dean <yiqund4.com>
2023-05-27 22:38:24 -0700

    everything except pagination

--- app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListActivity.java
+++ app/src/main/java/edu/uci/ics/fabflixmobile/ui/movielist/MovieListActivity.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import android.widget.Button;
 
+import com.android.volley.Response;
 import android.os.Bundle;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -34,6 +35,9 @@ public class MovieListActivity extends AppCompatActivity {
     private final String port = "8080";
     private final String domain = "fablix_war";
     private final String baseURL = "http://" + host + ":" + port + "/" + domain;
+    private ArrayList<Movie> movies;
+    private MovieListViewAdapter movieadapter;
+    private int pagenum = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +45,7 @@ public class MovieListActivity extends AppCompatActivity {
         // TODO: this should be retrieved from the backend server
         //final ArrayList<Movie> movies = new ArrayList<>();
         Log.d("wjbdj",baseURL + "/api/movies");
-        ArrayList<Movie> movies = getMovieList();
+        movies = getMovieList();
         //movies.add(new Movie("The Terminal", (short) 2004));
         //movies.add(new Movie("The Final Season", (short) 2007));
         /*
@@ -60,39 +64,33 @@ public class MovieListActivity extends AppCompatActivity {
 
          */
     }
-    protected ArrayList<Movie> getMovieList() {
-        ArrayList<Movie> movieList = new ArrayList<>();
-        final RequestQueue queue = NetworkManager.sharedManager(this).queue;
-        final StringRequest movieListRequest = new StringRequest(Request.Method.GET,
-                //actually use search. we need to get query from a search page
-                baseURL + "/api/movies?page=1&pagesize=10&sort=ranking&order=DESC",
-                response -> {
-                    try {
-                        JSONArray jsonArray = new JSONArray(response);
-                        String lastmovie = "";
-                        String prevstar = "";
-                        String prevgenre = "";
-                        int length = -1;
-                        for (int i = 0; i < jsonArray.length(); i++) {
-                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                            if (!jsonObject.getString("title").equals(lastmovie)) {
-                                String title = jsonObject.getString("title");
-                                Log.d("title",title);
-                                Log.d("lastmov",lastmovie);
-                                int year = jsonObject.getInt("year");
-                                String director = jsonObject.getString("director");
-                                ArrayList<String> starList = new ArrayList<String>();
-                                 starList.add(jsonObject.getString("star"));
-                                ArrayList<String> genreList = new ArrayList<String>();
-                                genreList.add(jsonObject.getString("genre"));
-                                movieList.add(new Movie(title, year, director, genreList, starList));
-                                // update prior
-                                lastmovie = jsonObject.getString("title");
-                                prevgenre = jsonObject.getString("genre");
-                                prevstar = jsonObject.getString("star");
-                                length += 1;
-                            }
-                            else{
+    protected void parseMovie(ArrayList<Movie> movieList, String response){
+
+    try {
+        JSONArray jsonArray = new JSONArray(response);
+        String lastmovie = "";
+        String prevstar = "";
+        String prevgenre = "";
+        int length = -1;
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            if (!jsonObject.getString("title").equals(lastmovie)) {
+                String title = jsonObject.getString("title");
+                Log.d("title", title);
+                Log.d("lastmov", lastmovie);
+                int year = jsonObject.getInt("year");
+                String director = jsonObject.getString("director");
+                ArrayList<String> starList = new ArrayList<String>();
+                starList.add(jsonObject.getString("star"));
+                ArrayList<String> genreList = new ArrayList<String>();
+                genreList.add(jsonObject.getString("genre"));
+                movieList.add(new Movie(title, year, director, genreList, starList));
+                // update prior
+                lastmovie = jsonObject.getString("title");
+                prevgenre = jsonObject.getString("genre");
+                prevstar = jsonObject.getString("star");
+                length += 1;
+            } else {
                                 /*
                                 if (resultdata[i].genre != prevgenre) {
                                     array[length].genres.push(resultdata[i].genre);
@@ -104,24 +102,62 @@ public class MovieListActivity extends AppCompatActivity {
                                     //console.log(resultdata[i].starid);
                                     }
                                  */
-                                if(jsonObject.getString("genre") != prevgenre){
-                                    movieList.get(length).getGenres().add(jsonObject.getString("genre"));
-                                    prevgenre = jsonObject.getString("genre");
-                                }
-                                if(jsonObject.getString("star") != prevstar){
-                                    movieList.get(length).getStars().add(jsonObject.getString("star"));
-                                    prevstar = jsonObject.getString("star");
-                                }
+                if (!jsonObject.getString("genre").equals(prevgenre)) {
+                    movieList.get(length).getGenres().add(jsonObject.getString("genre"));
+                    prevgenre = jsonObject.getString("genre");
+                }
+                if (!jsonObject.getString("star").equals(prevstar)) {
+                    movieList.get(length).getStars().add(jsonObject.getString("star"));
+                    prevstar = jsonObject.getString("star");
+                }
 
 
-                            }
+            }
+        }
+    }
+    catch(Exception e){
+        e.printStackTrace();
+    }
+    }
+    protected void searchMovie(String query){
+        ArrayList<Movie> movieList = new ArrayList<>();
+        Log.d("wdbwjdb",baseURL + "/search?page=1&pagesize=10&sort=ranking&order=DESC&title=" +query);
+        final RequestQueue queue = NetworkManager.sharedManager(this).queue;
+        final StringRequest movieListRequest = new StringRequest(
+                Request.Method.GET,
+                baseURL + "/search?page=1&pagesize=10&sort=ranking&order=DESC&title=" +query,
+                response -> {
+                    parseMovie(movieList,response);
+                    Log.d("searchedmovie",movieList.toString());
+                    movies.clear();
+                    movies.addAll(movieList);
+                    movieadapter.notifyDataSetChanged();
+                    Log.d("updatedmovies",movies.toString());
+                    // Handle response
+                },
+                error -> {
+                    // Handle error
+                    Log.e("Volley", "Error occurred", error);
+                }
+        );
+        queue.add(movieListRequest);
 
 
-                        }
+
+    }
+    protected ArrayList<Movie> getMovieList() {
+        ArrayList<Movie> movieList = new ArrayList<>();
+        final RequestQueue queue = NetworkManager.sharedManager(this).queue;
+        final StringRequest movieListRequest = new StringRequest(Request.Method.GET,
+                //actually use search. we need to get query from a search page
+                baseURL + "/api/movies?page=1&pagesize=10&sort=ranking&order=DESC",
+                response -> {
+                    try {
+                        parseMovie(movieList,response);
                         Log.d("movielist",movieList.toString());
-                        MovieListViewAdapter adapter = new MovieListViewAdapter(this, movieList);
+                        movieadapter = new MovieListViewAdapter(this, movieList);
                         ListView listView = findViewById(R.id.list);
-                        listView.setAdapter(adapter);
+                        listView.setAdapter(movieadapter);
                         listView.setOnItemClickListener((parent, view, position, id) -> {
                             Movie movie = movieList.get(position);
                             @SuppressLint("DefaultLocale") String message = String.format("Clicked on position: %d, name: %s, %d", position, movie.getName(), movie.getYear());
@@ -137,12 +173,24 @@ public class MovieListActivity extends AppCompatActivity {
                             public boolean onQueryTextSubmit(String query) {
                                 // Handle the search query here
                                 Log.d("Usersubmit", query);
+                                searchMovie(query);
                                 return true;
                             }
                             @Override
                             public boolean onQueryTextChange(String query) {
                                 Log.d("UserInput", query);
+
                                 return true;
+                            }
+                        });
+                        searchView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                if (searchView.hasFocus()) {
+                                    searchView.clearFocus();
+                                } else {
+                                    searchView.requestFocus();
+                                }
                             }
                         });
                         Button searchButton = findViewById(R.id.searchButton);
@@ -157,7 +205,7 @@ public class MovieListActivity extends AppCompatActivity {
                         });
 
                     }
-                    catch (JSONException e) {
+                    catch (Exception e) {
                         e.printStackTrace();
                     }
 
